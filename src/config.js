5be897a93c3cd2956e99a2fb05cdce1d96e6065e
harsh <harsh.chouhan010@gmail.com>
2023-10-02 02:46:41 -0700

    harsh final

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
 const config = {
-    apiKey : "https://bet-bitcoin.ai/api/",//process.env.REACT_APP_API_KEY,
+    apiKey : "https://api.bet-bitcoin.ai/api/",
     baseUrl:'/',
     projectId:'4de52e20ff4ec6762efc2e3ec4fb66d7',
    //contractAddress:"0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
